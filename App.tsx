064019371c42380fac8139da7e184d993e9a3f67
Sam Schooler <sam@airkit.com>
2021-02-11 00:03:28 -0700

    Code Example

--- App.tsx
+++ App.tsx
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { Button, InputAccessoryView, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TextInput } from "react-native";
+
+const Stack = createStackNavigator();
+
+const Screen1: React.FC = () => {
+  const navigation = useNavigation();
+  const inputRef = React.useRef<TextInput>(null);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      console.log("Focus Again");
+      inputRef.current?.focus();
+      // Hacky Solution: setTimeout(() => inputRef.current?.focus(), 300);
+    });
+    return unsubscribe;
+  }, [navigation, inputRef.current]);
+
+  return (
+    <View>
+      <Button
+        title="Go To Screen2"
+        onPress={() => navigation.navigate("Screen2")}
+      />
+      <InputAccessoryView>
+        <TextInput ref={inputRef} autoFocus value="Hi there" />
+      </InputAccessoryView>
+    </View>
+  );
+};
+
+const Screen2: React.FC = () => {
+  const { goBack } = useNavigation();
+  return (
+    <View>
+      <Button title="Go Back" onPress={goBack} />
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
